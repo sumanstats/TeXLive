60b8c9ed680de5d40594338138049a387f84b673
sumanstats <suman81765@gmail.com>
2021-09-20 23:13:35 +0545

    fixing error-2

--- build.ps1
+++ build.ps1
@@ -3,10 +3,10 @@ Write-Host "Starting build"
 
 cd $env:TEX_VER 
 $REPO=$env:TEX_VER.ToLower()
-$env:DOCKER_OPTS scheme="scheme-basic" -t $env:USER/$REPO:basic .
-$env:DOCKER_OPTS scheme="scheme-minimal" -t $env:USER/$REPO:minimal .
-$env:DOCKER_OPTS scheme="scheme-small" -t $env:USER/$REPO:small .
-$env:DOCKER_OPTS scheme="scheme-full" -t $env:USER/$REPO:full .
+$env:DOCKER_OPTS "scheme=scheme-basic" -t $env:USER/$REPO:basic .
+$env:DOCKER_OPTS "scheme=scheme-minimal" -t $env:USER/$REPO:minimal .
+$env:DOCKER_OPTS "scheme=scheme-small" -t $env:USER/$REPO:small .
+$env:DOCKER_OPTS "scheme=scheme-full" -t $env:USER/$REPO:full .
 docker tag $env:USER/$REPO:full $env:USER/$REPO:latest
 
 Write-Host "Finished build"

e99c1d2da2b46346ded6cd5940dbe2c493e66f66
sumanstats <suman81765@gmail.com>
2021-09-21 00:30:33 +0545

    fixing error-5

--- build.ps1
+++ build.ps1
@@ -3,10 +3,10 @@ Write-Host "Starting build"
 
 cd $env:TEX_VER 
 $REPO=$env:TEX_VER.ToLower()
-docker build --build-arg scheme=scheme-basic -t $env:USER/$REPO:basic .
-docker build --build-arg scheme=scheme-minimal -t $env:USER/$REPO:minimal .
-docker build --build-arg scheme=scheme-small -t $env:USER/$REPO:small .
-docker build --build-arg scheme=scheme-full -t $env:USER/$REPO:full .
-docker tag $env:USER/$REPO:full $env:USER/$REPO:latest
+docker build --build-arg scheme=scheme-basic -t $env:USER/${REPO}:basic .
+docker build --build-arg scheme=scheme-minimal -t $env:USER/${REPO}:minimal .
+docker build --build-arg scheme=scheme-small -t $env:USER/${REPO}:small .
+docker build --build-arg scheme=scheme-full -t $env:USER/${REPO}:full .
+docker tag $env:USER/${REPO}:full $env:USER/${REPO}:latest
 
 Write-Host "Finished build"
--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,7 @@
 $REPO=$env:TEX_VER.ToLower()
 docker login -u="$env:DOCKER_USERNAME" -p="$env:DOCKER_PASSWORD"
-docker push $env:USER/$REPO:basic
-docker push $env:USER/$REPO:minimal
-docker push $env:USER/$REPO:small
-docker push $env:USER/$REPO:full
-docker push $env:USER/$REPO:latest
+docker push $env:USER/${REPO}:basic
+docker push $env:USER/${REPO}:minimal
+docker push $env:USER/${REPO}:small
+docker push $env:USER/${REPO}:full
+docker push $env:USER/${REPO}:latest

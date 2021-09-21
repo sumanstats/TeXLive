cf7fd9a5b64a384d44927eb202643f7b30752021
sumanstats <suman81765@gmail.com>
2021-09-21 15:24:23 +0545

    build full and latest

--- build.ps1
+++ build.ps1
@@ -4,11 +4,11 @@ Write-Host "Starting build"
 $env:REPO=$env:TEX_VER.ToLower()
 cd $env:TEX_VER 
 
-docker build --build-arg scheme=scheme-minimal -t $env:USER/"$env:REPO":minimal .
-docker build --build-arg scheme=scheme-basic -t $env:USER/"$env:REPO":basic .
-docker build --build-arg scheme=scheme-small -t $env:USER/"$env:REPO":small .
+#docker build --build-arg scheme=scheme-minimal -t $env:USER/"$env:REPO":minimal .
+#docker build --build-arg scheme=scheme-basic -t $env:USER/"$env:REPO":basic .
+#docker build --build-arg scheme=scheme-small -t $env:USER/"$env:REPO":small .
 #multiple tags at once latest and full
-#docker build --build-arg scheme=scheme-full -t $env:USER/"$env:REPO":full -t $env:USER/"$env:REPO":latest .
+docker build --build-arg scheme=scheme-full -t $env:USER/"$env:REPO":full -t $env:USER/"$env:REPO":latest .
 
 
 cd $env:APPVEYOR_BUILD_FOLDER

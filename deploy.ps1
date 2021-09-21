e48d3c0cdbac9a127662fac9e604380fe1976fa5
sumanstats <suman81765@gmail.com>
2021-09-21 15:10:52 +0545

    build all tags except full and latest

--- deploy.ps1
+++ deploy.ps1
@@ -4,8 +4,8 @@ Write-Host "Starting deployment to Dockerhub"
 docker push $env:USER/"$env:REPO":minimal
 docker push $env:USER/"$env:REPO":basic
 docker push $env:USER/"$env:REPO":small
-docker push $env:USER/"$env:REPO":full
-docker push $env:USER/"$env:REPO":latest
+#docker push $env:USER/"$env:REPO":full
+#docker push $env:USER/"$env:REPO":latest
 
 Write-Host "Finished deployment to Dockerhub"
 

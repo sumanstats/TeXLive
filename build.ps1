d615d8eb71445bb4ad8c394e4aae8efc58c5d77f
sumanstats <suman81765@gmail.com>
2021-09-21 12:00:11 +0545

    fixing error-7

--- build.ps1
+++ build.ps1
@@ -5,9 +5,5 @@ cd $env:TEX_VER
 $REPO=$env:TEX_VER.ToLower()
 docker build --build-arg scheme=scheme-basic -t $env:USER/${REPO}:basic .
 docker build --build-arg scheme=scheme-minimal -t $env:USER/${REPO}:minimal .
-docker build --build-arg scheme=scheme-small -t $env:USER/${REPO}:small .
-docker build --build-arg scheme=scheme-full -t $env:USER/${REPO}:full .
-docker tag $env:USER/${REPO}:full $env:USER/${REPO}:latest
-cd ..
 
 Write-Host "Finished build"
--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,6 @@
+
+
 docker login -u="$env:DOCKER_USERNAME" -p="$env:DOCKER_PASSWORD"
 docker push $env:USER/${REPO}:basic
 docker push $env:USER/${REPO}:minimal
-docker push $env:USER/${REPO}:small
-docker push $env:USER/${REPO}:full
-docker push $env:USER/${REPO}:latest
+

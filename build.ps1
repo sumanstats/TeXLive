2cef8d211dbd80e644f77b29bfae29892df7bb38
sumanstats <suman81765@gmail.com>
2021-09-21 12:57:52 +0545

    fixing error-20

--- build.ps1
+++ build.ps1
@@ -2,11 +2,9 @@ $ErrorActionPreference = 'Stop';
 Write-Host "Starting build"
 
 $env:REPO=$env:TEX_VER.ToLower()
-echo $env:REPO
 cd $env:TEX_VER 
 docker build --build-arg scheme=scheme-minimal -t $env:USER/"$env:REPO":minimal .
 cd $env:APPVEYOR_BUILD_FOLDER
-docker login -u="$env:DOCKER_USERNAME" -p="$env:DOCKER_PASSWORD"
-docker push $env:USER/"$env:REPO":minimal
+
 
 Write-Host "Finished build"
--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,5 @@
-
+echo $env:REPO
+docker login -u="$env:DOCKER_USERNAME" -p="$env:DOCKER_PASSWORD"
+docker push $env:USER/"$env:REPO":minimal
 
 

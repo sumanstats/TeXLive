dd269cd7052c7df196c398494b8cb170bc617355
sumanstats <suman81765@gmail.com>
2021-09-21 14:19:15 +0545

    try multistage and multiple tags at once

--- build.ps1
+++ build.ps1
@@ -7,8 +7,9 @@ cd $env:TEX_VER
 docker build --build-arg scheme=scheme-minimal -t $env:USER/"$env:REPO":minimal .
 docker build --build-arg scheme=scheme-basic -t $env:USER/"$env:REPO":basic .
 docker build --build-arg scheme=scheme-small -t $env:USER/"$env:REPO":small .
-docker build --build-arg scheme=scheme-full -t $env:USER/"$env:REPO":full .
-docker tag $env:USER/"$env:REPO":full $env:USER/"$env:REPO":latest
+#multiple tags at once latest and full
+docker build --build-arg scheme=scheme-full -t $env:USER/"$env:REPO":full -t $env:USER/"$env:REPO":latest .
+
 
 cd $env:APPVEYOR_BUILD_FOLDER
 

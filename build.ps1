0f03266612dc7050dc46e918aa0c2c2069f65772
sumanstats <suman81765@gmail.com>
2021-09-21 13:08:11 +0545

    finally fixed !

--- build.ps1
+++ build.ps1
@@ -3,8 +3,13 @@ Write-Host "Starting build"
 
 $env:REPO=$env:TEX_VER.ToLower()
 cd $env:TEX_VER 
-docker build --build-arg scheme=scheme-minimal -t $env:USER/"$env:REPO":minimal .
-cd $env:APPVEYOR_BUILD_FOLDER
 
+docker build --build-arg scheme=scheme-minimal -t $env:USER/"$env:REPO":minimal .
+docker build --build-arg scheme=scheme-basic -t $env:USER/"$env:REPO":basic .
+docker build --build-arg scheme=scheme-small -t $env:USER/"$env:REPO":small .
+docker build --build-arg scheme=scheme-full -t $env:USER/"$env:REPO":full .
+docker tag $env:USER/"$env:REPO":full $env:USER/"$env:REPO":latest
+
+cd $env:APPVEYOR_BUILD_FOLDER
 
 Write-Host "Finished build"
--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,12 @@
-echo $env:REPO
 docker login -u="$env:DOCKER_USERNAME" -p="$env:DOCKER_PASSWORD"
+
+Write-Host "Starting deployment to Dockerhub"
 docker push $env:USER/"$env:REPO":minimal
+docker push $env:USER/"$env:REPO":basic
+docker push $env:USER/"$env:REPO":small
+docker push $env:USER/"$env:REPO":full
+docker push $env:USER/"$env:REPO":latest
+
+Write-Host "Finished deployment to Dockerhub"
 
 

ac3ce862f88f7faecd035abe1766e75b1e1dcdd5
sumanstats <suman81765@gmail.com>
2021-09-21 12:18:31 +0545

    fixing error-9

--- build.ps1
+++ build.ps1
@@ -1,9 +1,10 @@
 $ErrorActionPreference = 'Stop';
 Write-Host "Starting build"
 
-cd $env:TEX_VER 
 $REPO=$env:TEX_VER.ToLower()
+cd $env:TEX_VER 
 docker build --build-arg scheme=scheme-basic -t $env:USER/${REPO}:basic .
 docker build --build-arg scheme=scheme-minimal -t $env:USER/${REPO}:minimal .
+cd $env:APPVEYOR_BUILD_FOLDER
 
 Write-Host "Finished build"

a680871c998ac846d49b299afe76691128fddf75
sumanstats <suman81765@gmail.com>
2021-09-21 12:40:39 +0545

    fixing error-14

--- build.ps1
+++ build.ps1
@@ -1,11 +1,11 @@
 $ErrorActionPreference = 'Stop';
 Write-Host "Starting build"
 
-$REPO=$env:TEX_VER.ToLower()
+$env:REPO=$env:TEX_VER.ToLower()
 cd $env:TEX_VER 
-$($env:DOCKER_OPTS) scheme=scheme-minimal -t $env:USER/"$REPO":minimal .
+($env:DOCKER_OPTS) scheme=scheme-minimal -t $env:USER/$env:REPO:minimal .
 cd $env:APPVEYOR_BUILD_FOLDER
 docker login -u="$env:DOCKER_USERNAME" -p="$env:DOCKER_PASSWORD"
-docker push $env:USER/"$REPO":minimal
+docker push $env:USER/$env:REPO:minimal
 
 Write-Host "Finished build"

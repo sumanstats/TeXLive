8f39cef85f0fbe62f0e66cf0b2470a825643aff9
sumanstats <suman81765@gmail.com>
2021-09-21 12:27:22 +0545

    fixing error-10

--- build.ps1
+++ build.ps1
@@ -3,8 +3,7 @@ Write-Host "Starting build"
 
 $REPO=$env:TEX_VER.ToLower()
 cd $env:TEX_VER 
-docker build --build-arg scheme=scheme-basic -t $env:USER/${REPO}:basic .
-docker build --build-arg scheme=scheme-minimal -t $env:USER/${REPO}:minimal .
+docker build --build-arg scheme=scheme-minimal -t $env:USER/"$REPO":minimal .
 cd $env:APPVEYOR_BUILD_FOLDER
 
 Write-Host "Finished build"
--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,3 @@
 docker login -u="$env:DOCKER_USERNAME" -p="$env:DOCKER_PASSWORD"
-docker push $env:USER/${REPO}:basic
-docker push $env:USER/${REPO}:minimal
+docker push $env:USER/"$REPO":minimal
 

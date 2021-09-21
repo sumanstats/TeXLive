72bf6b3e6d78b526beb8f5531090fd55fdb26ff0
sumanstats <suman81765@gmail.com>
2021-09-21 12:34:06 +0545

    fixing error-12

--- build.ps1
+++ build.ps1
@@ -3,7 +3,7 @@ Write-Host "Starting build"
 
 $REPO=$env:TEX_VER.ToLower()
 cd $env:TEX_VER 
-docker build --build-arg scheme=scheme-minimal -t $env:USER/"$REPO":minimal .
+$env:DOCKER_OPTS scheme=scheme-minimal -t $env:USER/"$REPO":minimal .
 cd $env:APPVEYOR_BUILD_FOLDER
 docker login -u="$env:DOCKER_USERNAME" -p="$env:DOCKER_PASSWORD"
 docker push $env:USER/"$REPO":minimal

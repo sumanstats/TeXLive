e83a363541df35315908bfb7145e12e34185f703
Suman Khanal <suman81765@gmail.com>
2024-05-07 00:17:47 +0545

    pass tar xzf not zxf, looks weird, used to work previously

--- build.ps1
+++ build.ps1
@@ -8,13 +8,13 @@ cd $env:TEX_VER
 # docker build --build-arg scheme=scheme-basic -t $env:USER/"$env:REPO":basic .
 # docker build --build-arg scheme=scheme-small -t $env:USER/"$env:REPO":small .
 
-# docker build --build-arg scheme=scheme-minimal -t $env:USER/"$env:REPO":minimal .
-# docker build --build-arg scheme=scheme-basic -t $env:USER/"$env:REPO":basic .
-# docker build --build-arg scheme=scheme-small -t $env:USER/"$env:REPO":small .
+docker build --build-arg scheme=scheme-minimal -t $env:USER/"$env:REPO":minimal .
+docker build --build-arg scheme=scheme-basic -t $env:USER/"$env:REPO":basic .
+docker build --build-arg scheme=scheme-small -t $env:USER/"$env:REPO":small .
 
 
 # multiple tags at once latest and full
-docker build --build-arg scheme=scheme-full -t $env:USER/"$env:REPO":full -t $env:USER/"$env:REPO":latest .
+# docker build --build-arg scheme=scheme-full -t $env:USER/"$env:REPO":full -t $env:USER/"$env:REPO":latest .
 
 
 cd $env:APPVEYOR_BUILD_FOLDER
